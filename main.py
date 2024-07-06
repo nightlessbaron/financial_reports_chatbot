5a318209d2f151e932eef98f9b11cda78952b540
Varad <pimpalkhutevarad@gmail.com>
2024-07-06 09:13:43 +0000

    bugs in deploying online

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 os.environ["TOGETHER_API_KEY"] = st.secrets["TOGETHER_API_KEY"]
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
